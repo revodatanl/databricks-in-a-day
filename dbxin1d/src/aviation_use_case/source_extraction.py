42d6872ab4182195bd170f8b0d6ca0d0143f6abf
Kornel Kovacs - Koko <53813164+kovacskokokornel@users.noreply.github.com>
2024-05-08 09:05:09 +0200

    Update source_extraction.py
    Signed-off-by: Kornel Kovacs - Koko <53813164+kovacskokokornel@users.noreply.github.com>

--- dbxin1d/src/aviation_use_case/source_extraction.py
+++ dbxin1d/src/aviation_use_case/source_extraction.py
@@ -45,7 +45,7 @@ payload["flightList"][1]["to"] = extra_days_formatted
 
 # Hitting the API endpoint
 response = requests.post(
-    "https://be.wizzair.com/22.3.0/Api/search/timetable", headers=header, json=payload
+    "https://be.wizzair.com/22.4.0/Api/search/timetable", headers=header, json=payload
 )
 
 # Handling the response from the server
